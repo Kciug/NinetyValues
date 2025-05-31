8f007415eeb30efbda64e92dcb05026c2039ac70
RafalS <rafalskrzypczyk96@gmail.com>
2025-05-31 17:12:59 +0200

    dagger hilt initialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.dagger.hilt)
 }
 
 android {
@@ -56,4 +58,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.compiler)
+}
+
+kapt {
+    correctErrorTypes = true
 }
---
+++ app/src/main/java/com/rafalskrzypczyk/ninetyvalues/NinetyValuesApp.kt
@@ -0,0 +1,7 @@
+package com.rafalskrzypczyk.ninetyvalues
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NinetyValuesApp : Application()
