366c08ae55f84c716c1148a882a74f3a5fa78315
RafalS <rafalskrzypczyk96@gmail.com>
2025-06-14 03:33:40 +0200

    entries history screen rework + animation

--- app/src/main/java/com/rafalskrzypczyk/ninetyvalues/presentation/entries_history/EntriesHistoryScreen.kt
+++ app/src/main/java/com/rafalskrzypczyk/ninetyvalues/presentation/entries_history/EntriesHistoryScreen.kt
@@ -1,17 +1,23 @@
 package com.rafalskrzypczyk.ninetyvalues.presentation.entries_history
 
 import android.content.res.Configuration
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.CalendarMonth
@@ -23,8 +29,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,6 +51,12 @@ fun EntriesHistoryScreen(
     onNavigateBack: () -> Unit,
     onNavigateToEntry: (Long) -> Unit
 ) {
+    var visible by remember { mutableStateOf(false) }
+
+    LaunchedEffect(state.entries) {
+        if(state.entries.isNotEmpty()) visible = true
+    }
+
     Scaffold(
         topBar = {
             NavigationTopBar(
@@ -49,13 +67,18 @@ fun EntriesHistoryScreen(
     ) { innerPadding ->
         val modifier = Modifier.padding(innerPadding)
 
-        LazyColumn (
-            contentPadding = PaddingValues(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = modifier.fillMaxSize()
-        ) {
-            items(state.entries) { item ->
-                EntryCard(item) { id -> onNavigateToEntry(id) }
+        AnimatedVisibility(
+            visible = visible,
+            enter = fadeIn(animationSpec = tween(durationMillis = 300, delayMillis = 100))
+        ){
+            LazyColumn(
+                contentPadding = PaddingValues(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = modifier.fillMaxSize()
+            ) {
+                items(state.entries) { item ->
+                    EntryCard(item) { id -> onNavigateToEntry(id) }
+                }
             }
         }
     }
@@ -66,33 +89,51 @@ fun EntryCard(
     entry: Entry,
     onClick: (Long) -> Unit
 ) {
-    Card (
+    Card(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable(onClick = { onClick(entry.id) }),
+            .clip(RoundedCornerShape(20.dp))
+            .clickable { onClick(entry.id) },
         colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            containerColor = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.8f),
             contentColor = MaterialTheme.colorScheme.onPrimaryContainer
-        )
+        ),
     ) {
-        Row(
-            modifier = Modifier.padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 20.dp, vertical = 16.dp)
         ) {
-            Icon(
-                imageVector = Icons.Default.CalendarMonth,
-                contentDescription = stringResource(R.string.ic_desc_date)
-            )
-            Spacer(Modifier.width(16.dp))
-            Text(
-                text = entry.timestamp ?: "",
-                style = MaterialTheme.typography.titleLarge
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            Icon(
-                imageVector = Icons.AutoMirrored.Default.ArrowForward,
-                contentDescription = stringResource(R.string.ic_desc_open_entry),
-            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.CalendarMonth,
+                    contentDescription = stringResource(R.string.ic_desc_date),
+                )
+                Spacer(Modifier.width(12.dp))
+                Text(
+                    text = entry.timestamp ?: stringResource(R.string.missing_date),
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(R.string.open_entry),
+                    style = MaterialTheme.typography.labelMedium,
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Icon(
+                    imageVector = Icons.AutoMirrored.Default.ArrowForward,
+                    contentDescription = stringResource(R.string.open_entry),
+                )
+            }
         }
     }
 }
